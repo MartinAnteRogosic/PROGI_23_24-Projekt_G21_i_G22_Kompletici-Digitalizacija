f8519682a799fad7917695531c65ff8aa20f8314
vinko <vinko.grancic@fer.hr>
2024-01-09 21:59:28 +0100

    table for documents for processing

---
+++ backend/src/main/java/hr/fer/progi/backend/entity/DocumentsForProcessing.java
@@ -0,0 +1,32 @@
+package hr.fer.progi.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "DocumentForProcessing")
+public class DocumentsForProcessing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "document_id", nullable = false)
+    private DocumentEntity document;
+
+
+
+
+}
